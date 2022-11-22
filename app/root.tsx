b3cc7aa4ef8dc23788f9dc45268a986ebbdf02b8
jsix <mrjohn73@yandex.ru>
2022-11-22 15:10:24 +0300

    some

--- app/root.tsx
+++ app/root.tsx
@@ -1,40 +1,32 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import { createEmotionCache, MantineProvider } from "@mantine/core";
-import { StylesPlaceholder } from "@mantine/remix";
-import { theme } from "./theme";
+import type { MetaFunction } from '@remix-run/node';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { createEmotionCache, MantineProvider } from '@mantine/core';
+import { StylesPlaceholder } from '@mantine/remix';
+import { theme } from './theme';
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+    charset: 'utf-8',
+    viewport: 'width=device-width,initial-scale=1',
 });
 
-createEmotionCache({ key: "mantine" });
+createEmotionCache({ key: 'mantine' });
 
 export default function App() {
-  return (
-    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-      <html lang="en">
-        <head>
-          <StylesPlaceholder />
-          <Meta />
-          <Links />
-        </head>
-        <body>
-          <Outlet />
-          <ScrollRestoration />
-          <Scripts />
-          <LiveReload />
-        </body>
-      </html>
-    </MantineProvider>
-  );
+    return (
+        <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+            <html lang="ru">
+                <head>
+                    <StylesPlaceholder />
+                    <Meta />
+                    <Links />
+                </head>
+                <body>
+                    <Outlet />
+                    <ScrollRestoration />
+                    <Scripts />
+                    <LiveReload />
+                </body>
+            </html>
+        </MantineProvider>
+    );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,15 @@
-import { Text, Button, Stack } from "@mantine/core";
+import { Text, Button, Stack } from '@mantine/core';
+import Layout from '~/layouts/Layout';
 
 export default function Index() {
-  return (
-    <Stack align="center" mt={50}>
-      <Text size="xl" weight={500}>
-        Welcome to Mantine!!!
-      </Text>
-      <Button>Click the button</Button>
-    </Stack>
-  );
+    return (
+        <Layout>
+            <Stack align="center" mt={50}>
+                <Text size="xl" weight={500}>
+                    Welcome to Mantine!!!
+                </Text>
+                <Button>Click the button</Button>
+            </Stack>
+        </Layout>
+    );
 }
